a4839117d9cfdfd5bb475af8f8414b554642a5c8
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-08-25 21:18:25 -0500

    cambiando puerto y agregando cords

--- src/main/java/com/prueba/app/controller/ClienteController.java
+++ src/main/java/com/prueba/app/controller/ClienteController.java
@@ -4,6 +4,7 @@ import javax.net.ssl.HttpsURLConnection;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import com.prueba.app.service.ClienteService;
 
 @RestController
 @RequestMapping("/api/clientes")
+@CrossOrigin(origins = "http://localhost:4200") 
 public class ClienteController {
 
     private final ClienteService clienteService;
